df0fb0c0044d932ad97f92df564b5cbfacb9bff0
yuvi-raj <yuvayuvaraj12999@gmail.com>
2021-12-10 22:36:57 +0530

    hello

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,37 @@
+package org.add;
+
+public class GreensTech {
+
+	private void Omr() {
+		System.out.println("Omr");
+
+	}
+	private void Adayar() {
+		System.out.println("Adayar");
+
+	}
+	private void Tambaram() {
+		System.out.println("Tambaram");
+
+	}
+	private void Velacherry() {
+		System.out.println("Velacherry");
+
+	}
+	private void Annanagar() {
+		System.out.println("Annanagar");
+
+	}
+	public static void main(String[] args) {
+		
+		GreensTech Greens = new GreensTech ();
+		
+		Greens.Omr();
+		Greens.Adayar();
+		Greens.Tambaram();
+		Greens.Velacherry();
+		Greens.Annanagar();
+		
+	}
+
+}
